3c181b03c5fcfe3e47fb7413bb48d91f1d35b9df
adamlaarais <adam.laarais@uha.fr>
2025-01-09 05:10:55 +0100

    modified

--- js/SAE.js
+++ js/SAE.js
@@ -15,7 +15,17 @@ if (nbr && SAE[nbr]) {
 
     document.querySelector(".sae").innerHTML = `
         <div>
-            <div class="sae-nbr">${nbr}</div>
+            <div class="sae-nbr">
+                <a class="retour" href="listeSAE.html">
+                <div class="svg">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M19 12H6M12 5l-7 7 7 7"/></svg>
+                </div>
+                </a>
+                <div class="sae-centre">
+                    <div>${nbr}</div>
+                </div>
+            </div>
             <div class="sae-titre">${sae.titre}</div>
             <div class="sae-description">${sae.description}</div>
             <div class="sae-compétences">${sae.compétences}</div>
